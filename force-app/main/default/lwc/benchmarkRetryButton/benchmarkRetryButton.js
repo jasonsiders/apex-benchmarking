1c1566500e6b8cd2f6017095c46959ee72048d38
Jason Siders <41714875+jasonsiders@users.noreply.github.com>
2025-01-28 10:24:45 -0600

    rough draft of component logic

--- force-app/main/default/lwc/benchmarkRetryButton/benchmarkRetryButton.js
+++ force-app/main/default/lwc/benchmarkRetryButton/benchmarkRetryButton.js
@@ -1,11 +1,43 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { getRecord } from "lightning/uiRecordApi";
+import jobNameField from "@salesforce/schema/Benchmark__c.JobName__c";
+import launch from "@salesforce/apex/BenchmarkLaunchUtility.launch";
 
 export default class BenchmarkRetryButton extends LightningElement {
-	@api invoke() {
-		console.log(`@jason: hello world!`);
+	@api recordId;
+
+	@api async invoke() {
+		try {
+			const jobId = await launch({ developerName: this.jobName });
+			this.showSuccessToast(jobId);
+		} catch (error) {
+			console.error(error);
+			this.showErrorToast(error);
+		}
+	}
+
+	@wire(getRecord, { recordId: "$recordId", fields: [jobNameField] })
+	record;
+
+	get jobName() {
+		const fieldName = jobNameField?.fieldApiName;
+		return this.record ? this.record?.data?.fields[fieldName]?.value : undefined;
+	}
+
+	showErrorToast(error) {
 		const event = new ShowToastEvent({
-			title: "Hello World!",
+			title: "Something went wrong...",
+			message: error,
+			variant: "error"
+		});
+		this.dispatchEvent(event);
+	}
+
+	showSuccessToast(jobId) {
+		const event = new ShowToastEvent({
+			title: `Launched ${this.jobName} via ${jobId}`,
+			message: "A new Benchmark record will be created in a few moments...",
 			variant: "success"
 		});
 		this.dispatchEvent(event);
